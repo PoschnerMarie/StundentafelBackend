f4ee116b29dd4aaa28cf83d875bebbddbcb27917
PoschnerMarie <marie.poschner@edu.siemens-energy.com>
2022-06-01 15:41:39 +0200

    connection to mysql db

--- src/serverfile/server.js
+++ src/serverfile/server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ".Sheld!ieic8265",
+  database: "Stundentafel"
+});
+
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
+
 var server = app.listen(8081, function () {
    //var host = server.address().address
    var host = 'localhost';
@@ -10,4 +19,18 @@ var server = app.listen(8081, function () {
    console.log(host)
    
    console.log("Example app listening at http://%s:%s", host, port)
+
+
+
+   connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
 })
+
+
+
+
