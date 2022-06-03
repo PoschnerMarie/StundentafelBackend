0a586085b57a0ff5c9ebe02e54236ccc93dbffa3
PoschnerMarie <marie.poschner@edu.siemens-energy.com>
2022-06-03 11:59:26 +0200

    added endpoints lehrer, klassen

--- src/serverfile/server.js
+++ src/serverfile/server.js
@@ -8,22 +8,52 @@ var connection = mysql.createConnection({
   database: "Stundentafel"
 });
 
-app.get('/test', function (req, res) {
+const cors = require('cors')
+
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+
+app.use(cors(corsOptions))
+
+app.get('/lehrer', function (req, res) {
   var query = connection.query("SELECT * FROM Lehrer", function(err, result, fields){
     if (err) throw err;
     // if there is no error, you have the result
     // iterate for all the rows in result
+    /*
+    var row;
     Object.keys(result).forEach(function(key) {
-      var row = result[key];
+      row = result[key];
       console.log(row);
       console.log(row.Kuerzel);
       console.log(row.Abwesenheitsnotiz);
       console.log(row.Fortbildungs_ID)
+      
     });
+    res.send(row);
+    */
+    res.send(result);
   });
-  
-  res.send('Hello World');
 })
+
+app.get('/klassen', function (req, res) {
+  var query = connection.query("SELECT * FROM Klasse", function(err, result, fields){
+    if (err) throw err;
+    console.log(result);
+    res.send(result);
+  });
+})
+/*
+app.post('/addklasse', function requestHandler(req, res) {
+  var query = connection.query("insert into Klasse(K_Bezeichnung, Kuerzel) VALUES ('FS192', 'MAR')", function(err, result){
+    if (err) throw err;
+    console.log(result);
+    req.flash('success', 'Data added successfully!');
+    res.redirect('/');
+  });
+})*/
 
 function connectMySQL(){
   connection.connect(function(err) {
